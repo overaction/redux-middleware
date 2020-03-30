ec8080bec030c6347e1f4b638598f45586d12ae1
overaction <kmc04@naver.com>
2020-03-30 14:07:05 +0000

    만들어진 미들웨어 사용하기

--- src/index.js
+++ src/index.js
@@ -6,9 +6,10 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import rootReducer from './modules';
-import loggerMiddleware from './lib/loggerMiddleware';
+import {createLogger} from 'redux-logger';
 
-const store = createStore(rootReducer, applyMiddleware(loggerMiddleware));
+const logger = createLogger();
+const store = createStore(rootReducer, applyMiddleware(logger));
 
 ReactDOM.render(
   <Provider store={store}>
