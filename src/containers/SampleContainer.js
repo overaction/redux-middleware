b75edf381138338350c4a4ab5d47b8a9cf203ea2
overaction <kmc04@naver.com>
2020-04-01 14:56:51 +0000

    SampleContainer with Hooks

--- src/containers/SampleContainer.js
+++ src/containers/SampleContainer.js
@@ -1,3 +1,35 @@
+import React, { useCallback, useEffect } from 'react';
+import {useSelector, useDispatch} from 'react-redux';
+import {getPost, getUsers} from '../modules/sample';
+import Sample from '../components/Sample';
+
+const SampleContainer = () => {
+    const {post, users, loadingPost, loadingUsers} = useSelector(({sample}) => ({
+        post: sample.post,
+        users: sample.users,
+        loadingPost: sample.loading.GET_POST,
+        loadingUsers: sample.loading.GET_USERS
+    }));
+    const dispatch = useDispatch();
+    const getpost = useCallback(id => dispatch(getPost(id)),[dispatch]);
+    const getusers = useCallback(() => dispatch(getUsers()),[dispatch]);
+    useEffect(() => {
+        getpost(1);
+        getusers();
+    },[getPost, getUsers])
+    return (
+        <Sample
+            post={post}
+            users={users}
+            loadingPost={loadingPost}
+            loadingUsers={loadingUsers}
+        />
+    )
+}
+export default SampleContainer;
+
+/*
+
 import React, { useEffect } from 'react';
 import {connect} from 'react-redux';
 import Sample from '../components/Sample';
@@ -38,36 +70,4 @@ export default connect(
     }
 )(SampleContainer);
 
-
-
-/*
-import React, { useCallback, useEffect } from 'react';
-import {useSelector, useDispatch} from 'react-redux';
-import {getPost, getUsers} from '../modules/sample';
-import Sample from '../components/Sample';
-
-const SampleContainer = () => {
-    const {post, users, loadingPost, loadingUsers} = useSelector(({sample}) => ({
-        post: sample.post,
-        users: sample.users,
-        loadingPost: sample.loading.GET_POST,
-        loadingUsers: sample.loading.GET_USERS
-    }));
-    const dispatch = useDispatch();
-    const getPost = useCallback(id => dispatch(getPost(id)),[dispatch]);
-    const getUsers = useCallback(() => dispatch(getUsers()),[dispatch]);
-    useEffect(() => {
-        getPost(1);
-        getUsers(1);
-    },[getPost, getUsers])
-    return (
-        <Sample
-            post={post}
-            users={users}
-            loadingPost={loadingPost}
-            loadingUsers={loadingUsers}
-        />
-    )
-}
-export default SampleContainer;
 */
