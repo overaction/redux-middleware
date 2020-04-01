bd83355c78d4a73f28d06069ea7de2818464bc04
overaction <kmc04@naver.com>
2020-04-01 14:49:48 +0000

    SampleContainer 미완성2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-//import SampleContainer from './containers/SampleContainer';
-import CounterContainer from './containers/CounterContainer';
+import SampleContainer from './containers/SampleContainer';
+//import CounterContainer from './containers/CounterContainer';
 
 const App = () => {
     return (
         <div>
-            <CounterContainer />
+            <SampleContainer />
         </div>
     )
 }
